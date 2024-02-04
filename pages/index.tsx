ae895ef5fae859e217ea9acb75fc354503755665
Dinesh Rao <dinopuppala@gmail.com>
2024-02-03 19:01:14 -0800

    te

--- pages/index.tsx
+++ pages/index.tsx
@@ -177,9 +177,10 @@ const Home: NextPage = () => {
           )}
         </div>
         <div>
-    <h2
+        <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
                   ref={bioRef}
+                >
                   Mastering APA Citation and Formatting: A Detailed Guide</h2>
     <p>This comprehensive guide is designed to help you understand and apply the 7th edition of the APA (American Psychological Association) formatting and citation style. It includes essential information on referencing, various citation formats for different types of sources, and additional useful tips.</p>
 
